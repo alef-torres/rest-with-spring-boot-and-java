571b7588ecfac38843210724b1f8406f075b538b
Alef <aleftorresbr@gmail.com>
2025-11-14 14:28:13 -0300

    Atualizando o projeto do curso com novas classes

---
+++ src/main/java/com/github/alef_torres/math/SimpleMath.java
@@ -0,0 +1,19 @@
+package com.github.alef_torres.math;
+
+public class SimpleMath {
+    public Double sum(Double numberOne, Double numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+    public Double sub(Double numberOne, Double numberTwo) {
+        return numberOne - numberTwo;
+    }
+
+    public Double multi(Double numberOne, Double numberTwo) {
+        return numberOne * numberTwo;
+    }
+
+    public Double div(Double numberOne, Double numberTwo) {
+        return numberOne / numberTwo;
+    }
+}
